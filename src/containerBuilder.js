c416e4035f2138457d129402cb2216f505500dba
Raif Harik <reharik@gmail.com>
2015-12-09 12:58:17 -0600

    Update containerBuilder.js

--- src/containerBuilder.js
+++ src/containerBuilder.js
@@ -16,24 +16,16 @@ module.exports = function(registryFunc, containerFunc){
     var dependencies = R.uniqWith((a,b) => a.name == b.name, dto.dependencyDeclarations)
         .concat(dto.overrideDeclarations.filter(x=>!x.newName));
 
-// need to compose this better cuz it's fucked
     var renames = R.filter(x=>x.newName, dto.overrideDeclarations);
 
-    var f = x=> {
+    var rename = x=> {
         var clone  = R.clone(dependencies.find(d=>d.name == x.name));
         clone.name = x.newName;
         return clone;
     };
     //var result = R.concat(renames.map(f), dependencies);
-
-    var result = R.map(f,renames);
-
-
-
-    console.log('xxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
-    console.log(result);
-
-    //
-    //return containerFunc(new ContainerDSL(dependencies));
+    var dependencies = R.map(rename, renames);
+    
+    var instantiations = containerFunc(new InstantiateDSL(dependencies))
 
 };
