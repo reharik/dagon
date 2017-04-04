78c8f822945eeced18e76cbc24a52276d879873c
Raif Harik <reharik@gmail.com>
2017-04-04 10:02:50 -0500

    fixed logger

--- src/logger.js
+++ src/logger.js
@@ -3,42 +3,42 @@ var moment = require('moment');
 
 module.exports = function () {
   var transports = [];
-    transports.push(
-      new (winston.transports.Console)({
-        handleExceptions: true,
-        prettyPrint: true,
-        colorize: true,
-        silent: false,
-        timestamp: true,
-        json: false,
-        formatter: function (x) {
-          return '[' + x.level + '] ' + ' module: DAGon msg: ' + x.message + ' | ' + moment().format('h:mm:ss a');
-        }
-      })
-    );
-  winston.configure({
+  transports.push(
+    new (winston.transports.Console)({
+      handleExceptions: true,
+      prettyPrint: true,
+      colorize: true,
+      silent: false,
+      timestamp: true,
+      json: false,
+      formatter: function (x) {
+        return '[' + x.level + '] ' + ' module: DAGon msg: ' + x.message + ' | ' + moment().format('h:mm:ss a');
+      }
+    })
+  );
+
+  var logger = new winston.Logger({
     transports,
     level: process.env.DAGON_LOGGING_LEVEL || 'error'
   });
-
   var trace = (message) => {
-    winston.silly(message);
+    logger.silly(message);
   };
 
   var debug = (message) => {
-    winston.debug(message);
+    logger.debug(message);
   };
 
   var info = (message) => {
-    winston.info(message);
+    logger.info(message);
   };
 
   var warn = (message) => {
-    winston.warn(message);
+    logger.warn(message);
   };
 
   var error = (message) => {
-    winston.error(message);
+    logger.error(message);
   };
 
   return {
@@ -50,4 +50,3 @@ module.exports = function () {
   }
 }();
 
-  
